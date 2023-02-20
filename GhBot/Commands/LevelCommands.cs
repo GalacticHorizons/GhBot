04d35c43a8ff4a1615949ca533da86d6772816f0
ollie <ollie@ollierobinson.co.uk>
2023-02-20 15:48:14 +0000

    pretty much finished rank card

--- GhBot/Commands/LevelCommands.cs
+++ GhBot/Commands/LevelCommands.cs
@@ -21,8 +21,6 @@ public class LevelCommands : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("level", "Get your current level.")]
     public async Task ShowLevel(SocketGuildUser member = null)
     {
-        bool memberNull = member == null;
-        
         member ??= (SocketGuildUser) Context.User;
 
         if (member.IsBot)
@@ -34,24 +32,26 @@ public class LevelCommands : InteractionModuleBase<SocketInteractionContext>
         await DeferAsync();
 
         Member dbMember = await Data.Data.GetMember(member.Id);
-
-        if (dbMember == null)
-        {
-            await FollowupAsync($"{(memberNull ? "You haven't" : (member.Nickname ?? member.Username) + " hasn't")} sent any messages yet!");
-            return;
-        }
-
-        using ImageSurface surface = new ImageSurface(Format.Rgb24, 700, 250);
+        dbMember ??= new Member();
+        
+        using ImageSurface surface = new ImageSurface(Format.Rgb24, 600, 200);
         using Context c = new Context(surface);
+        
+        RoundRect(c, 5, 5, surface.Width - 10, surface.Height - 10, 30);
+        c.Clip();
 
         ImageSurface galaxy = new ImageSurface("Content/galaxy.png");
         c.SetSourceSurface(galaxy, -195, -383);
         c.Paint();
         galaxy.Dispose();
+        
+        RoundRect(c, 20, 20, surface.Width - 40, surface.Height - 40, 30);
+        c.SetSourceRGBA(0, 0, 0, 0.5);
+        c.Fill();
 
-        const double circX = 125;
-        const double circY = 250 / 2f;
-        const double radius = 100;
+        const double circX = 100;
+        const double circY = 200 / 2f;
+        const double radius = 70;
         const double imgSize = radius * 2 + 5;
         
         c.Arc(circX, circY, radius, 0, MathF.PI * 2);
@@ -76,23 +76,105 @@ public class LevelCommands : InteractionModuleBase<SocketInteractionContext>
         
         c.Restore();
         c.ResetClip();
-        c.Arc(circX, circY, radius, 0, MathF.PI * 2);
+        /*c.Arc(circX, circY, radius, 0, MathF.PI * 2);
         c.SetSourceRGB(0, 0, 0);
         c.LineWidth = 5;
-        c.Stroke();
+        c.Stroke();*/
 
+        const double userPosX = 180;
+        const double userPosY = 75;
+
+        c.SelectFontFace("sans-serif", FontSlant.Normal, FontWeight.Normal);
         c.SetSourceRGB(1, 1, 1);
-        c.Rectangle(0, 0, 700, 250);
+        int fontSize = 40;
+        c.SetFontSize(fontSize);
+        string text = member.Nickname ?? member.Username;
+        TextExtents extents = c.TextExtents(text);
+
+        const int maxWidth = 285;
+        // The level circle takes up space that could be used by the text. So if the text's width is larger than its space,
+        // keep reducing its size until it fits.
+        while (extents.Width >= maxWidth)
+        {
+            fontSize -= 1;
+            c.SetFontSize(fontSize);
+            extents = c.TextExtents(text);
+
+            // If, however, the font size goes below a certain level (25 is about as low as can reasonably be gone while
+            // still looking decent), then trim the string to fit.
+            if (fontSize < 23)
+            {
+                string checkStr = "" + text[0];
+                int i = 1;
+                extents = c.TextExtents(checkStr);
+                TextExtents extentsOfEllipsis = c.TextExtents("...");
+                while (extents.Width < maxWidth - extentsOfEllipsis.Width)
+                {
+                    checkStr += text[i++];
+                    extents = c.TextExtents(checkStr);
+                }
+
+                text = checkStr + "...";
+                
+                break;
+            }
+        }
+        
+        c.MoveTo(userPosX, userPosY);
+        c.ShowText(text);
+        c.SetSourceRGB(0.6, 0.6, 0.6);
+        c.MoveTo(userPosX, userPosY + 30);
+        c.SelectFontFace("sans-serif", FontSlant.Normal, FontWeight.Bold);
+        c.SetFontSize(25);
+        c.ShowText("#" + member.Discriminator);
+
+        uint maxLvlMsgs = (dbMember.Level * 5 + 5);
+        uint lvlMsgs = dbMember.LvlMsgs;
+
+        const double progressX = userPosX;
+        const double progressY = 130;
+        const double progressW = 375;
+        const double progressH = 30;
+        const double progressR = progressH / 2d;
+        
+        RoundRect(c, progressX, progressY, progressW, progressH, progressR);
+        c.SetSourceRGB(0, 0, 0);
+        c.FillPreserve();
+        c.Clip();
+
+        RoundRect(c, progressX - progressW + (progressW * lvlMsgs / maxLvlMsgs), progressY, progressW, progressH, progressR);
+        c.SetSourceRGB(102 / 255.0, 51 / 255.0, 153 / 255.0);
         c.Fill();
 
-        //RoundRect(c, 100, 100, 100, 100, 20);
-        RandomPath(c, 100);
+        text = lvlMsgs + "/" + maxLvlMsgs + " XP";
+        extents = c.TextExtents(text);
+        c.MoveTo(progressX + progressW / 2 - (extents.Width / 2 + extents.XBearing), progressY + progressH / 2 - (extents.Height / 2 + extents.YBearing));
+        c.SetSourceRGB(1, 1, 1);
+        c.ShowText(text);
+
+        c.ResetClip();
+        RoundRect(c, progressX, progressY, progressW, progressH, progressR);
         c.SetSourceRGB(0, 0, 0);
+        c.LineWidth = 2;
         c.Stroke();
+
+        const double lvlX = 525;
+        const double lvlY = 75;
+        const double lvlR = 40;
         
-        c.SelectFontFace("Serif", FontSlant.Normal, FontWeight.Normal);
-        
-        Console.WriteLine(c.Status);
+        c.Arc(lvlX, lvlY, lvlR, 0, Math.PI * 2);
+        c.SetSourceRGB(0.2, 0.2, 0.2);
+        c.FillPreserve();
+        c.SetSourceRGB(0, 0, 0);
+        c.LineWidth = 4;
+        c.Stroke();
+
+        c.SetFontSize(50);
+        text = dbMember.Level.ToString();
+        extents = c.TextExtents(text);
+        c.MoveTo(lvlX - (extents.Width / 2 + extents.XBearing), lvlY - (extents.Height / 2 + extents.YBearing));
+        c.SetSourceRGB(1, 1, 1);
+        c.ShowText(text);
 
         ImageWriter writer = new ImageWriter();
         using MemoryStream stream = new MemoryStream();
@@ -113,26 +195,19 @@ public class LevelCommands : InteractionModuleBase<SocketInteractionContext>
 
     private static void RoundRect(Context c, double x, double y, double width, double height, double r)
     {
-        double halfR = r / 2;
+        const double halfRadians = Math.PI;
+        const double quarterRadians = Math.PI / 2;
         
         c.NewPath();
-        c.Arc(x + halfR, y + halfR, r, Math.PI, -Math.PI / 2);
-        c.LineTo(x + r + width, y - r);
-        c.Arc(x + r + width, y, r, -Math.PI / 2, 0);
-        c.LineTo(x + r * 2 + width, y + r + height);
-        c.Arc(x + r + width, y + r + height, r, 0, Math.PI / 2);
-        c.LineTo(x, y + r * 2 + width);
-        c.Arc(x, y + r + height, r, Math.PI / 2, Math.PI);
-        c.LineTo(x, y);
-        
-        c.ClosePath();
-    }
+        c.ArcNegative(r + x, r + y, r, -quarterRadians, halfRadians);
+        c.LineTo(x, y + height - r);
+        c.ArcNegative(r + x, height - r + y, r, halfRadians, quarterRadians);
+        c.LineTo(x + width - r, y + height);
+        c.ArcNegative(x + width - r, y + height - r, r, quarterRadians, 0);
+        c.LineTo(x + width, y + r);
+        c.ArcNegative(x + width - r, y + r, r, 0, -quarterRadians);
+        c.LineTo(x + r, y);
 
-    public static void RandomPath(Context c, int numIterations)
-    {
-        for (int i = 0; i < numIterations; i++)
-        {
-            c.LineTo(Random.Shared.Next(0, 700), Random.Shared.Next(0, 250));
-        }
+        c.ClosePath();
     }
 }
